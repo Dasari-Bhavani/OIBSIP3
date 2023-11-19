10d1d087924ce0d23bba8bde047ab2315e13da2b
Dasari-Bhavani <151420928+Dasari-Bhavani@users.noreply.github.com>
2023-11-19 10:18:04 +0530

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,19 @@
+function convertTemperature(){
+    const inputTemperature=parseFloat(document.getElementById("num").value);
+    const unit=document.getElementById("unit").value;
+
+    let convertedTemperature;
+    if(unit== 'celsius'){
+        //convert to Fahrenheit
+        convertedTemperature=(inputTemperature * 9/5) + 32;
+        document.getElementById("result").textContent=`${inputTemperature} degrees Celsius=${convertedTemperature} degrees Fahrenheit.`
+    }
+    else if(unit==='Fahrenheit'){
+        //convert to celsius
+        convertedTemperature=(inputTemperature-32)* 5/9;
+        document.getElementById("result").textContent=`${inputTemperature} degrees Fahrenheit= ${convertedTemperature} degrees Celsius.`
+    }
+    else{
+        document.getElementById("result").textContent='Please Select a VALID unit.';
+    }
+}
